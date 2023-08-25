57c460f5d32c7484772fc20c7f02926fd70ff0db
Peter-Michael Osera <psosera@gmail.com>
2023-08-25 13:49:20 -0500

    Added localStorage support in IDE

--- src/web/ide.ts
+++ src/web/ide.ts
@@ -7,6 +7,57 @@ const editorPane = document.getElementById('editor')
 const outputPane = document.getElementById('output')
 const runButton  = document.getElementById('run')
 
+// https://developer.mozilla.org/en-US/docs/Web/API/Web_Storage_API/Using_the_Web_Storage_API
+function storageAvailable(type: any) {
+  let storage;
+  try {
+    storage = window[type]
+    const x = '__storage_test__';
+    (storage as any).setItem(x, x)
+    (storage as any).removeItem(x)
+    return true
+  } catch (e) {
+    return (
+      e instanceof DOMException &&
+      // everything except Firefox
+      (e.code === 22 ||
+        // Firefox
+        e.code === 1014 ||
+        // test name field too, because code might not be present
+        // everything except Firefox
+        e.name === "QuotaExceededError" ||
+        // Firefox
+        e.name === "NS_ERROR_DOM_QUOTA_REACHED") &&
+      // acknowledge QuotaExceededError only if there's something already stored
+      storage &&
+      storage.length !== 0
+    )
+  }
+}
+
+// N.B., local files are a simple object mapping filenames to (string) contents
+let files: object = {}
+let isStorageAvailable = storageAvailable('localStorage')
+let currentFile: string | null = null
+
+function initializeStorage() {
+  if (isStorageAvailable) {
+    if (localStorage.getItem('scamper-files') !== null) {
+      files = JSON.parse(localStorage.getItem('scamper-files')!) as object
+    }
+    if (localStorage.getItem('scamper-last-opened-file') !== null) {
+      currentFile = localStorage.getItem('scamper-last-current-file')!
+    }
+  }
+}
+
+function commitStorage() {
+  if (isStorageAvailable) {
+    localStorage.setItem('scamper-files', JSON.stringify(files))
+    localStorage.setItem('scamper-last-opened-file', currentFile!)
+  }
+}
+
 function initialize() {
   editor = new EditorView({
     doc: `
