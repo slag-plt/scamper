9e7d407bbabbc7a31f3372f1a289c3d1b1206b10
Peter-Michael Osera <psosera@gmail.com>
2023-09-08 07:06:14 -0700

    Attempts at cleaning up Lezer language. Confused.

--- src/codemirror/language.ts
+++ src/codemirror/language.ts
@@ -1,12 +1,21 @@
 import {parser} from "./syntax.grammar"
-import {LRLanguage, LanguageSupport, indentNodeProp, foldNodeProp, foldInside, delimitedIndent} from "@codemirror/language"
+import {LRLanguage, LanguageSupport, indentNodeProp, foldNodeProp, foldInside, continuedIndent, delimitedIndent} from "@codemirror/language"
 import {styleTags, tags as t} from "@lezer/highlight"
 
 export const ScamperLanguage = LRLanguage.define({
   parser: parser.configure({
     props: [
       indentNodeProp.add({
-        Application: delimitedIndent({closing: ")", align: false})
+        Import: continuedIndent({except: undefined, units: 1}),
+        Define: continuedIndent({except: undefined, units: 1}),
+        Display: continuedIndent({except: undefined, units: 1}),
+        Lambda: continuedIndent({except: undefined, units: 1}),
+        arglist: continuedIndent({except: undefined, units: 1}),
+        Application: continuedIndent({except: undefined, units: 1}),
+        And: continuedIndent({except: undefined, units: 1}),
+        Or: continuedIndent({except: undefined, units: 1}),
+        Cond: continuedIndent({except: undefined, units: 1}),
+        binding: continuedIndent({except: undefined, units: 1}),
       }),
       foldNodeProp.add({
         Application: foldInside
@@ -28,6 +37,7 @@ export const ScamperLanguage = LRLanguage.define({
     commentTokens: {line: ";"}
   }
 })
+
 
 export function ScamperSupport() {
   return new LanguageSupport(ScamperLanguage)
