96f811a152cb61d8128e555bccabb6beafcdbf12
Peter-Michael Osera <osera@cs.grinnell.edu>
2025-09-19 03:11:43 -0500

    Augmented CLI to take arguments

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -2,6 +2,8 @@ import * as Scheme from '../scheme'
 import builtinLibs from '../lib'
 import * as LPM from '../lpm'
 import TextRenderer from '../lpm/renderers/text-renderer'
+
+import { parseArgs } from 'node:util'
 import fs from 'fs'
 
 class ConsoleOutput implements LPM.OutputChannel, LPM.ErrorChannel {
@@ -16,11 +18,42 @@ class ConsoleOutput implements LPM.OutputChannel, LPM.ErrorChannel {
   }
 }
 
-const src = fs.readFileSync(process.stdin.fd, 'utf-8');
+////////////////////////////////////////////////////////////////////////////////
+
+const { values, positionals } = parseArgs({
+  options: {
+    help: {
+      type: 'boolean',
+      short: '?',
+      default: false,
+    },
+    trace: {
+      type: 'boolean',
+    },
+    // Define other options as needed
+  },
+  allowPositionals: true, // Set to true if your CLI accepts positional arguments
+});
+
+if (values.help || positionals.length !== 1) {
+  console.log('Usage: scamper [options] filename')
+  console.log('Options:')
+  console.log('  -?, --help       Show this help message')
+  console.log('  --trace          Enabling step-by-step tracing')
+  process.exit(0)
+}
+
+const filename = positionals[0]
+
+const src = fs.readFileSync(filename, 'utf-8');
 const out = new ConsoleOutput()
 const program = Scheme.compile(out, src)
 if (program === undefined) { process.exit(1) }
+
+const options = LPM.defaultOptions
+options.isTracing = values.trace ?? false
+
 const machine = new LPM.Machine(builtinLibs, new Map([
   ['scheme', Scheme.raiseThread] 
-]), Scheme.mkInitialEnv(), program, out, out)
+]), Scheme.mkInitialEnv(), program, out, out, options)
 machine.evaluate()
