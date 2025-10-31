ebeab2c7f0406dc30a8eda27f2a50b57465e52c9
Peter-Michael Osera <osera@cs.grinnell.edu>
2025-10-31 18:26:10 -0500

    Refactor calls by scale

--- src/lib/data/viz.ts
+++ src/lib/data/viz.ts
@@ -9,14 +9,12 @@ const viz: L.Library = new L.Library()
 
 interface Dataset extends L.Struct {
   [L.structKind]: 'dataset'
-  scale: 'linear' | 'category'
   opts: object
 }
 
 export function datasetQ (v: L.Value): boolean {
   return L.isStructKind(v, 'dataset')
 }
-viz.registerValue('dataset?', datasetQ)
 
 interface Plot extends L.Struct {
   [L.structKind]: 'plot'
@@ -26,13 +24,9 @@ interface Plot extends L.Struct {
 export function plotQ (v: L.Value): boolean {
   return L.isStructKind(v, 'plot')
 }
-viz.registerValue('plot?', plotQ)
 
-export function plot (...args: L.Value[]): Plot {
-  // TODO: hand-check arguments: datasets with an optional opts record
-  const datasets: Dataset[] = args as Dataset[]
-  // TODO: check to ensure dataasets all have consistent x-axis scales
-  const scale = datasets[0].scale
+export function plotLinear (...datasets: Dataset[]): Plot {
+  checkContract(arguments, contract('plot-linear', [], C.struct('dataset')))
   return {
     [L.scamperTag]: 'struct',
     [L.structKind]: 'plot',
@@ -42,67 +36,88 @@ export function plot (...args: L.Value[]): Plot {
       },
       options: {
         scales: {
-          x: { type: scale }
+          x: { type: 'linear' }
         }
       }
     }
   }
 }
-viz.registerValue('plot', plot)
+
+function plotCategory (labels: L.List, ...datasets: Dataset[]): Plot {
+  checkContract(arguments, contract('plot-category', [C.listof(C.string)], C.struct('dataset')))
+  return {
+    [L.scamperTag]: 'struct',
+    [L.structKind]: 'plot',
+    opts: {
+      data: {
+        labels: L.listToVector(labels) as string[],
+        datasets: datasets.map(ds => ds.opts)
+      },
+      options: {
+        scales: {
+          x: { type: 'category' }
+        }
+      }
+    }
+  }
+}
+
+function plotRadial (labels: L.List, ...datasets: Dataset[]): Plot {
+  checkContract(arguments, contract('plot-radial', [C.listof(C.string)], C.struct('dataset')))
+  return {
+    [L.scamperTag]: 'struct',
+    [L.structKind]: 'plot',
+    opts: {
+      data: {
+        labels: L.listToVector(labels) as string[],
+        datasets: datasets.map(ds => ds.opts)
+      }
+    }
+  }
+}
+
+function makeDataset (type: string, label: string, data: any[]): Dataset {
+  return {
+    [L.scamperTag]: 'struct',
+    [L.structKind]: 'dataset',
+    opts: { type, label, data, }
+  }
+}
 
 export function datasetLine (title: string, data: L.List): Dataset {
   checkContract(arguments, contract('dataset-line', [
     C.string,
-    C.listof(C.pairof(C.or(C.number, C.string), C.number))
+    C.listof(C.or(C.number, C.pairof(C.number, C.number)))
   ]))
-  const points: {x: number | string, y: number}[] = L.listToVector(data).map(v => {
-    const p = v as L.Pair
-    return { x: p.fst as number | string, y: p.snd as number }
+  const points: (number | {x: number, y: number})[] = L.listToVector(data).map(v => {
+    if (L.isPair(v)) {
+      return { x: v.fst as number, y: v.snd as number }
+    } else {
+      return v as number
+    }
   })
   if (points.length === 0) {
     throw new L.ScamperError('Runtime', 'dataset-line requires at least one data point')
   }
-  const scale = typeof points[0].x === 'string' ? 'category' : 'linear'
-  return {
-    [L.scamperTag]: 'struct', 
-    [L.structKind]: 'dataset',
-    scale,
-    opts: {
-      type: 'line',
-      label: title,
-      data: points,
-    }
-  }
+  return makeDataset('line', title, points)
 }
-viz.registerValue('dataset-line', datasetLine)
 
 export function datasetBar (title: string, data: L.List): Dataset {
   checkContract(arguments, contract('plot-bar', [
     C.string,
-    C.listof(C.pairof(C.or(C.string), C.number))
+    C.listof(C.number)
   ]))
-  const points: {x: number | string, y: number}[] = L.listToVector(data).map(v => {
-    const p = v as L.Pair
-    return { x: p.fst as number | string, y: p.snd as number }
-  })
+  const points: number[] = L.listToVector(data) as number[]
   if (points.length === 0) {
     throw new L.ScamperError('Runtime', 'dataset-bar requires at least one data point')
   }
-  return {
-    [L.scamperTag]: 'struct', 
-    [L.structKind]: 'dataset',
-    scale: 'category',
-    opts: {
-      type: 'bar',
-      label: title,
-      data: points,
-    }
-  }
+  return makeDataset('bar', title, points)
 }
-viz.registerValue('dataset-bar', datasetBar)
 
 export function datasetScatter (title: string, data: L.List): Dataset {
-  checkContract(arguments, contract('dataset-scatter', [C.string, C.listof(C.pairof(C.number, C.number))]))
+  checkContract(arguments, contract('dataset-scatter', [
+    C.string, C.listof(C.pairof(C.number, C.number))
+  ]))
   const points: {x: number | string, y: number}[] = L.listToVector(data).map(v => {
     const p = v as L.Pair
     return { x: p.fst as number, y: p.snd as number }
@@ -110,21 +125,14 @@ export function datasetScatter (title: string, data: L.List): Dataset {
   if (points.length === 0) {
     throw new L.ScamperError('Runtime', 'dataset-scatter requires at least one data point')
   }
-  return {
-    [L.scamperTag]: 'struct', 
-    [L.structKind]: 'dataset',
-    scale: 'linear',
-    opts: {
-      type: 'scatter',
-      label: title,
-      data: points,
-    }
-  }
+  return makeDataset('scatter', title, points)
 }
-viz.registerValue('dataset-scatter', datasetScatter)
 
 export function datasetBubble (title: string, data: L.List): Dataset {
-  checkContract(arguments, contract('dataset-bubble', [C.string, C.listof(C.listof(C.number))]))
+  checkContract(arguments, contract('dataset-bubble', [
+    C.string,
+    C.listof(C.listof(C.number))
+  ]))
   const points: {x: number, y: number, r: number}[] = L.listToVector(data).map(v => {
     const l = L.listToVector(v as L.List)
     if (l.length !== 3) {
@@ -135,47 +143,44 @@ export function datasetBubble (title: string, data: L.List): Dataset {
   if (points.length === 0) {
     throw new L.ScamperError('Runtime', 'dataset-bubble requires at least one data point')
   }
-  return {
-    [L.scamperTag]: 'struct',
-    [L.structKind]: 'dataset',
-    scale: 'linear',
-    opts: {
-      type: 'bubble',
-      label: title,
-      data: points,
-    }
-  }
+  return makeDataset('bubble', title, points)
 }
-viz.registerValue('dataset-bubble', datasetBubble)
 
 export function datasetPie (title: string, data: L.List): Dataset {
-  checkContract(arguments, contract('dataset-pie', [C.string, C.listof(C.pairof(C.string, C.number))]))
-  const labels: string[] = []
-  const values: number[] = []
-  L.listToVector(data).forEach(v => {
-    const p = v as L.Pair
-    labels.push(p.fst as string)
-    values.push(p.snd as number)
-  })
+  checkContract(arguments, contract('dataset-pie', [
+    C.string,
+    C.listof(C.number)
+  ]))
+  const values = L.listToVector(data) as number[]
   if (values.length === 0) {
     throw new L.ScamperError('Runtime', 'dataset-pie requires at least one data point')
   }
-  return {
-    [L.scamperTag]: 'struct',
-    [L.structKind]: 'dataset',
-    scale: 'category',
-    opts: {
-      type: 'pie',
-      label: title,
-      data: values,
-      labels
-    }
-  }
+  return makeDataset('pie', title, values)
 }
-viz.registerValue('dataset-pie', datasetPie)
 
-export function dataset
+export function datasetPolar (title: string, data: L.List): Dataset {
+  checkContract(arguments, contract('dataset-polar', [
+    C.string,
+    C.listof(C.number)
+  ]))
+  const values = L.listToVector(data) as number[]
+  if (values.length === 0) {
+    throw new L.ScamperError('Runtime', 'dataset-polar requires at least one data point')
+  }
+  return makeDataset('polarArea', title, values )
+}
 
+export function datasetRadar (title: string, data: L.List): Dataset {
+  checkContract(arguments, contract('dataset-radar', [
+    C.string,
+    C.listof(C.number)
+  ]))
+  const values = L.listToVector(data) as number[]
+  if (values.length === 0) {
+    throw new L.ScamperError('Runtime', 'dataset-radar requires at least one data point')
+  }
+  return makeDataset('radar', title, values)
+}
 
 addCustomWebRenderer(plotQ, (v: L.Value): HTMLElement => {
   const canvas = document.createElement('canvas')
@@ -187,5 +192,18 @@ addCustomWebRenderer(plotQ, (v: L.Value): HTMLElement => {
   new Chart(canvas, plot.opts as any)
   return canvas
 })
+
+viz.registerValue('dataset?', datasetQ)
+viz.registerValue('plot?', plotQ)
+viz.registerValue('plot-linear', plotLinear)
+viz.registerValue('plot-category', plotCategory)
+viz.registerValue('plot-radial', plotRadial)
+viz.registerValue('dataset-line', datasetLine)
+viz.registerValue('dataset-bar', datasetBar)
+viz.registerValue('dataset-scatter', datasetScatter)
+viz.registerValue('dataset-bubble', datasetBubble)
+viz.registerValue('dataset-pie', datasetPie)
+viz.registerValue('dataset-polar', datasetPolar)
+viz.registerValue('dataset-radar', datasetRadar)
 
 export default viz
