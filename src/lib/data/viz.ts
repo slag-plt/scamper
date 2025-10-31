bb9be4685f7c93de2b56806d9b983bb440d412a6
Peter-Michael Osera <osera@cs.grinnell.edu>
2025-10-31 08:38:04 -0500

    checkpoint

--- src/lib/data/viz.ts
+++ src/lib/data/viz.ts
@@ -33,7 +33,6 @@ export function plot (...args: L.Value[]): Plot {
   const datasets: Dataset[] = args as Dataset[]
   // TODO: check to ensure dataasets all have consistent x-axis scales
   const scale = datasets[0].scale
-  console.log(datasets[0])
   return {
     [L.scamperTag]: 'struct',
     [L.structKind]: 'plot',
@@ -123,6 +122,60 @@ export function datasetScatter (title: string, data: L.List): Dataset {
   }
 }
 viz.registerValue('dataset-scatter', datasetScatter)
+
+export function datasetBubble (title: string, data: L.List): Dataset {
+  checkContract(arguments, contract('dataset-bubble', [C.string, C.listof(C.listof(C.number))]))
+  const points: {x: number, y: number, r: number}[] = L.listToVector(data).map(v => {
+    const l = L.listToVector(v as L.List)
+    if (l.length !== 3) {
+      throw new L.ScamperError('Runtime', 'Data for dataset-bubble must be a list of three numbers')
+    }
+    return { x: l[0] as number, y: l[1] as number, r: l[2] as number }
+  })
+  if (points.length === 0) {
+    throw new L.ScamperError('Runtime', 'dataset-bubble requires at least one data point')
+  }
+  return {
+    [L.scamperTag]: 'struct',
+    [L.structKind]: 'dataset',
+    scale: 'linear',
+    opts: {
+      type: 'bubble',
+      label: title,
+      data: points,
+    }
+  }
+}
+viz.registerValue('dataset-bubble', datasetBubble)
+
+export function datasetPie (title: string, data: L.List): Dataset {
+  checkContract(arguments, contract('dataset-pie', [C.string, C.listof(C.pairof(C.string, C.number))]))
+  const labels: string[] = []
+  const values: number[] = []
+  L.listToVector(data).forEach(v => {
+    const p = v as L.Pair
+    labels.push(p.fst as string)
+    values.push(p.snd as number)
+  })
+  if (values.length === 0) {
+    throw new L.ScamperError('Runtime', 'dataset-pie requires at least one data point')
+  }
+  return {
+    [L.scamperTag]: 'struct',
+    [L.structKind]: 'dataset',
+    scale: 'category',
+    opts: {
+      type: 'pie',
+      label: title,
+      data: values,
+      labels
+    }
+  }
+}
+viz.registerValue('dataset-pie', datasetPie)
+
+export function dataset
+
 
 addCustomWebRenderer(plotQ, (v: L.Value): HTMLElement => {
   const canvas = document.createElement('canvas')
