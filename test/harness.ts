7e9e44e0a6740aaae40e9393df3b1fbcf1b98af4
Julian Kim <kimjulian@grinnell.edu>
2026-02-28 16:11:54 -0600

    Create image tracing regression test, add ARIA label to Scamper canvas, create test harness for HTMLDisplay

--- test/harness.ts
+++ test/harness.ts
@@ -2,6 +2,7 @@ import { expect, test } from 'vitest'
 import builtinLibs from '../src/lib'
 import * as Scheme from '../src/scheme'
 import * as LPM from '../src/lpm'
+import HTMLDisplay from "../src/lpm/output/html";
 
 export function runProgram (src: string, ops: LPM.Options = LPM.defaultOptions): string[] {
     src = src.trim()
@@ -21,6 +22,26 @@ export function runProgram (src: string, ops: LPM.Options = LPM.defaultOptions):
     )
     machine.evaluate()
     return out.log as string[]
+}
+
+export function runProgramWithHTML (src: string, out: HTMLDisplay, ops: LPM.Options = LPM.defaultOptions): HTMLElement[] {
+    src = src.trim()
+    const env = Scheme.mkInitialEnv()
+    const prog = Scheme.compile(out, src)
+
+    if (out.levels.length > 1) { return out.levels }
+    const machine = new LPM.Thread(
+      'test',
+      env,
+      prog!,
+      ops,
+      builtinLibs,
+      out,
+      out,
+      new Map([['scheme', Scheme.raiser]])
+    )
+    machine.evaluate()
+    return out.levels
 }
 
 export function scamperTest (label: string, src: string, expected: string[]) {
