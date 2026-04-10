6807258ac7b179cbc5024025d3011db5885734b4
Julian Kim <kimjulian@grinnell.edu>
2026-04-10 16:24:41 -0500

    Add Vue support in ESLint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,11 +6,21 @@ import tseslint from "typescript-eslint"
 import globals from "globals"
 import eslintConfigPrettier from "eslint-config-prettier/flat"
 import "eslint-plugin-only-warn"
+import vueEslint from "eslint-plugin-vue"
 
 export default defineConfig(
   eslint.configs.recommended,
   tseslint.configs.strictTypeChecked,
   tseslint.configs.stylisticTypeChecked,
+  ...vueEslint.configs["flat/recommended-error"],
+  {
+    files: ["*.vue", "**/*.vue"],
+    languageOptions: {
+      parserOptions: {
+        parser: tseslint.parser,
+      },
+    },
+  },
   eslintConfigPrettier,
   [globalIgnores(["dist/*", "types/*", "src/lib/webaudiofont/*"])],
   {
