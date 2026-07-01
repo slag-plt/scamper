0bb961cb1ae58b9afbc98c4261277cd017a26345
AsterSailor <142462619+AsterSailor@users.noreply.github.com>
2026-07-01 14:06:27 -0500

    if we were to fix cons we do smth with this

--- src/lpm/thread.ts
+++ src/lpm/thread.ts
@@ -172,6 +172,7 @@ export class Thread {
         this.draw()
       } else {
         this.out.send(mkTraceOutput(this.results[this.curStmt]))
+        //this.out.send(this)
       }
       
     }
