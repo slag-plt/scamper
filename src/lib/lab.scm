c821367404f285aa247e0ded188c65f5e3769c34
AsterSailor <142462619+AsterSailor@users.noreply.github.com>
2026-07-29 10:26:43 -0500

    reactive and labs relative tags

--- src/lib/lab.scm
+++ src/lib/lab.scm
@@ -1,23 +1,23 @@
 ;;; (title text) -> html?
 ;;;  text : string?
 ;;; Returns a title element.
-;;; @category formatting, html?
+;;; @category formatting, html?, part, problem, description
 (define title (js-var "lab_title"))
 
 ;;; (part text) -> html?
 ;;;  text : string?
 ;;; Returns a part element.
-;;; @category formatting, html?
+;;; @category formatting, html?, title, problem, description
 (define part (js-var "lab_part"))
 
 ;;; (problem text) -> html?
 ;;;  text : string?
 ;;; Returns a problem element.
-;;; @category formatting, html?
+;;; @category formatting, html?, title, part, description
 (define problem (js-var "lab_problem"))
 
 ;;; (description text) -> html?
 ;;;  text : string?
 ;;; Returns a description element.
-;;; @category formatting, html?
+;;; @category formatting, html?, title, part, problem
 (define description (js-var "lab_description"))
